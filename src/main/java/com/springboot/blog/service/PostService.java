d5e10c3835b66b4f697c299d8dc08ccd1e4dcc07
zakihaidari <zakihaidari914@gmail.com>
2025-02-08 21:38:47 +1100

    Initial commit - adding project files

---
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -0,0 +1,19 @@
+package com.springboot.blog.service;
+
+import com.springboot.blog.payload.PostDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPost();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById( long id);
+}
